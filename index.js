cbd3f50a3938c41f4e1d4f689a92ff8380921e58
ashleyphillips <ashleylphil@gmail.com>
2018-01-19 11:18:23 -0600

    comments

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ let friends = ['Kenna', 'Cody', 'Lydia', 'Jean', 'Laura'];
 
 document.getElementById('button').addEventListener('click', function () {
     for (let f = 0; f < friends.length; f++) {
-        let newDiv = document.createElement('div');
-        document.body.appendChild(newDiv);
-        newDiv.className = 'friends';
-        let newH3 = document.createElement('h3');
-        newDiv.appendChild(newH3);
-        let text = document.createTextNode(friends[f]);
-        newH3.appendChild(text);
+        let newDiv = document.createElement('div');//create div in html
+        document.body.appendChild(newDiv);//sends div to body of html
+        newDiv.className = 'friends';//assigning a class to the new div
+        let newH3 = document.createElement('h3');//creates h3 elements as variable 'newH3'
+        newDiv.appendChild(newH3);//puts h3 elements in newDiv
+        let text = document.createTextNode(friends[f]);//createTextNode lets doc know there will be text inside tag
+        newH3.appendChild(text);//puts the above text in h3 headings
         for (let i = 99; i > 0; i--){
             if (i === 2) {
-                let verse = document.createElement('p');
-                newDiv.appendChild(verse);
-                let verseText = document.createTextNode(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1} line of code in the file`);
-                verse.appendChild(verseText);
+                let verse = document.createElement('p');//creates 'p' tag and names it verse. you need it to be named something (verse) so you can refer to it later
+                newDiv.appendChild(verse);//puts verse in newDiv
+                let verseText = document.createTextNode(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1} line of code in the file`);//createTextNode puts text in something. here, it puts the lyrics in 'verseText'
+                verse.appendChild(verseText);//assigns verseText to verse
             } else if (i === 1) {
                 let verse = document.createElement('p');
                 newDiv.appendChild(verse);
