d4e5cfcaec7138c6f773275ad3e5c6e9fba86746
ashleyphillips <ashleylphil@gmail.com>
2018-01-19 10:37:51 -0600

    html in js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    console.log('DOM loaded');
+
+
 let friends = ['Kenna', 'Cody', 'Lydia', 'Jean', 'Laura'];
 
 document.getElementById('button').addEventListener('click', function () {
     for (let f = 0; f < friends.length; f++) {
-        console.log(`${friends[f].toUpperCase()}:`)
+        let newDiv = document.createElement('div');
+        document.body.appendChild(newDiv);
+        newDiv.className = 'friends';
+        let newH3 = document.createElement('h3');
+        newDiv.appendChild(newH3);
+        let text = document.createTextNode(friends[f]);
+        newH3.appendChild(text);
         for (let i = 99; i > 0; i--){
             if (i === 2) {
-                console.log(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1} line of code in the file`);
+                let verse = document.createElement('p');
+                newDiv.appendChild(verse);
+                let verseText = document.createTextNode(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1} line of code in the file`);
+                verse.appendChild(verseText);
             } else if (i === 1) {
-                console.log(`${i} line of code in the file, ${i} line of code; ${friends[f]} strikes one out, clears it all out, no more lines of code in the file`);
+                let verse = document.createElement('p');
+                newDiv.appendChild(verse);
+                let verseText = document.createTextNode(`${i} line of code in the file, ${i} line of code; ${friends[f]} strikes one out, clears it all out, no more lines of code in the file`);
+                verse.appendChild(verseText);
             } else {
-                console.log(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1} lines of code in the file`);
+                let verse = document.createElement('p');
+                newDiv.appendChild(verse);
+                let verseText = document.createTextNode(`${i} lines of code in the file, ${i} lines of code; ${friends[f]} strikes one out, clears it all out, ${i-1} lines of code in the file`);
+                verse.appendChild(verseText);
             }
         }
     }
 })
+});
